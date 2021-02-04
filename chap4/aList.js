b582ca13e61b86ab9520c307a4ecd4c02915b65f
Alex Langevin <alexlangev@gmail.com>
2021-02-04 16:44:40 -0500

    list exercices completed

--- chap4/aList.js
+++ chap4/aList.js
@@ -2,55 +2,52 @@
 //A list or sequence in programming is a data type the represents a 
 //countable number of ordered values.
 
-const { type } = require("os");
+let testArray = [1,2,3,4,5];
 
-let testArray = [1, 2, 3]
+const arrayToList = (array) => {
+  let result = {};
+  let restObject = null;
 
-const arrayToList = (arr) => {
-  let list = {};
-  let tempObject = null;
-
-  for(let i = arr.length - 1; i >= 0; i--){
-    list = {
-      value: testArray[i],
-      rest: tempObject,
+  for(let i = array.length - 1; i >= 0; i--){
+    result = {
+      value: array[i],
+      rest: restObject,
     }
-    tempObject = list;
+    restObject = result;
   }
-  return tempObject;
+  return result;
 }
-
-// console.log(arrayToList(testArray));
 
 const listToArray = (list) => {
   let result = [];
-
-  if( list.rest === undefined || typeof list === undefined || list.value === undefined ){
-    return result;
-  } else {
-    for (let node = list; node; node = node.rest){
-      if(node.hasOwnProperty('value')){
-        result.push(node.value);
-      }
+  if(list.value !== undefined || list.value !== null){
+    for(let node = list; node; node = node.rest){
+      result.push(node.value);
     }
   }
   return result;
 }
 
-console.log(listToArray(arrayToList(testArray)));
-
-/*
-function listToArrayWithHints(list) {
-  let result = [];
-  if (typeof list === 'undefined' || list.value === undefined || list.rest === undefined) {
-    return result;
-  } else {
-    for (let node = list; node; node = node.rest) {
-      if (node.hasOwnProperty('value')) {
-        result.push(node.value);
-      }
-    }
+const prepend = (element, list) => {
+  return {
+    value: element,
+    rest: list,
   }
-  return result;
 }
-*/
+
+const nth = (list, num) => {
+  if(list.value !== undefined || list.value !== null){
+    let counter = num;
+    let tempValue = list.value;
+    let tempRest = list.rest;
+
+    while(counter > 0) {
+      counter--;
+      tempValue = tempRest.value;
+      tempRest = tempRest.rest;
+    } 
+    return tempValue;
+  }
+}
+
+console.log(nth(arrayToList(testArray), 4))
